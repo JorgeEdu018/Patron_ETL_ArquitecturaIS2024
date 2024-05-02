44f622292a799184a03d4824d605c6c6ad742ec9
ElanTic <aplicacionesdejon@gmail.com>
2024-05-02 13:26:18 -0600

    corregido extractor txt y agregado transformador txt a loader.py

--- gestor-de-datos-master/src/transformers/txt_transformer.py
+++ gestor-de-datos-master/src/transformers/txt_transformer.py
@@ -15,37 +15,36 @@ from os.path import join
 import luigi, os, csv, json, re
 
 class TXTransformer(luigi.Task):
-     def requires(self):
+    def requires(self):
         return TXTExtractor()
 
     def run(self):
         result = []
         for file in self.input():
             with file.open() as txt_file:
-                lines = txt_file.readlines()
-                for line in lines:
-                    # Dividir la línea en campos usando "|" como separador
-                    fields = line.strip().split("|")
-                    # Crear un diccionario para cada línea
+                data = txt_file.read()
+                # Split rows by ';'
+                rows = data.strip().split(';')
+                for i in len(1, rows):
+                    # Split fields by ','
+                    fields = row[i].strip().split(',')
+                    # Create a dictionary for each row
                     entry = {
-                        "description": fields[0],
-                        "quantity": fields[1],
-                        "price": fields[2],
-                        "total": float(fields[1]) * float(fields[2]),
-                        "invoice": fields[3],
-                        "provider": fields[4],
-                        "country": fields[5]
+                        "description": fields[2],
+                        "quantity": fields[3],
+                        "price": fields[5],
+                        "total": float(fields[3]) * float(fields[5]),
+                        "invoice": fields[0],
+                        "provider": fields[6],
+                        "country": fields[7]
                     }
                     result.append(entry)
 
         with self.output().open('w') as out:
-            out.write(json.dumps(result, indent=4))
+            out.write(json.dumps(result, indent =4))
 
     def output(self):
-        project_dir = os.path.dirname(os.path.abspath(__file__))
+        project_dir = os.path.dirname(os.path.abspath("loader.py"))
         result_dir = join(project_dir, "result")
         return luigi.LocalTarget(join(result_dir, "txt.json"))
 
-
-if __name__ == "__main__":
-    luigi.build([TextFileTransformer()], local_scheduler=True)
