3092d91abdeba140b9c94a580df7e5b1104e9d7b
CCajero <89540480+CCajero@users.noreply.github.com>
2024-05-01 22:36:31 -0600

    Corrección de error en nombre de variable.

--- gestor-de-datos-master/src/extractors/txt_extractor.py
+++ gestor-de-datos-master/src/extractors/txt_extractor.py
@@ -1,15 +1,3 @@
-##!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------
-# Archivo: htm_extractor.py
-# Capitulo: Flujo de Datos
-# Autor(es): Perla Velasco & Yonathan Mtz. & Jorge Solís
-# Version: 1.0.0 Noviembre 2022
-# Descripción:
-#
-#   Este archivo define un procesador de datos que se encarga de extraer
-#   el contenido de un archivo HTM
-#-------------------------------------------------------------------------
 import luigi, os
 from os.path import isfile, join
 from src.readers.zip_reader import ZIPReader
@@ -25,6 +13,7 @@ class TXTExtractor(luigi.Task):
         files = [f for f in os.listdir(assets_dir) if isfile(join(assets_dir, f))]
         htm_files = [f for f in files if f.endswith(".txt")]
         targets = []
+        
         for file in htm_files:
             targets.append(luigi.LocalTarget(join(assets_dir, file)))
         return targets
