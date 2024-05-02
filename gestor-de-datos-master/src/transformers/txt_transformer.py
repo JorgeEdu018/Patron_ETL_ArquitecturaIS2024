b96f6e9851d9f33b9498e5b5531e071e53ee4d1c
JorgeEdu018 <77705299+JorgeEdu018@users.noreply.github.com>
2024-05-01 21:49:06 -0600

    Add files via upload

---
+++ gestor-de-datos-master/src/transformers/txt_transformer.py
@@ -0,0 +1,39 @@
+from src.extractors.txt_extractor import TXTExtractor
+from os.path import join
+import luigi, os, csv, json, re
+
+class TXTransformer(luigi.Task):
+     def requires(self):
+        return TXTExtractor()
+
+    def run(self):
+        result = []
+        for file in self.input():
+            with file.open() as txt_file:
+                lines = txt_file.readlines()
+                for line in lines:
+                    # Dividir la línea en campos usando "|" como separador
+                    fields = line.strip().split("|")
+                    # Crear un diccionario para cada línea
+                    entry = {
+                        "description": fields[0],
+                        "quantity": fields[1],
+                        "price": fields[2],
+                        "total": float(fields[1]) * float(fields[2]),
+                        "invoice": fields[3],
+                        "provider": fields[4],
+                        "country": fields[5]
+                    }
+                    result.append(entry)
+
+        with self.output().open('w') as out:
+            out.write(json.dumps(result, indent=4))
+
+    def output(self):
+        project_dir = os.path.dirname(os.path.abspath(__file__))
+        result_dir = join(project_dir, "result")
+        return luigi.LocalTarget(join(result_dir, "txt.json"))
+
+
+if __name__ == "__main__":
+    luigi.build([TextFileTransformer()], local_scheduler=True)
